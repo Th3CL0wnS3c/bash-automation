e7e15dc8cd0d1f73f6c92ee3b570e51f8eb39143
Th3CL0wnS3c <63302806+Th3CL0wnS3c@users.noreply.github.com>
2021-04-18 13:27:00 +0200

    Update debian-install-docker.sh
    update errors in variable name in echos (changed UPPER to lower case for GREEN and RST)

--- debian-install-docker.sh
+++ debian-install-docker.sh
@@ -36,39 +36,39 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo -e "\n${green}====${rst} Installing Docker dependencies ${GREEN}====${RST}\n"
+echo -e "\n${green}====${rst} Installing Docker dependencies ${green}====${rst}\n"
 sudo apt-get install -y  apt-transport-https ca-certificates curl gnupg2 software-properties-common 1> /dev/null 2> $errfile &
 progress
 check-err
 
-echo -e "${green}====${rst} Retrieving GPG Key from docker.com ${GREEN}====${RST}\n"
+echo -e "${green}====${rst} Retrieving GPG Key from docker.com ${green}====${rst}\n"
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 1> /dev/null 2>> $errfile &
 progress
 check-err
 
-echo -e "${green}====${rst} Adding docker sources to repositories list${GREEN}====${RST}\n"
+echo -e "${green}====${rst} Adding docker sources to repositories list${green}====${rst}\n"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 1> /dev/null 2>> $errfile &
 progress
 check-err
 sudo apt-get update 1> /dev/null 2>> $errfile
 check-err
 
-echo -e "${green}====${rst} Installing Docker ${GREEN}====${RST}\n"
+echo -e "${green}====${rst} Installing Docker ${green}====${rst}\n"
 sudo apt install -y docker-ce-cli docker-ce containerd.io 1> /dev/null 2>> $errfile &
 progress
 check-err
 
-echo -e "${green}====${rst} Installing docker-compose Docker Compose ${GREEN}====${RST}\n"
+echo -e "${green}====${rst} Installing docker-compose Docker Compose ${green}====${rst}\n"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 1> /dev/null 2>> $errfile &
 progress
 check-err
 sudo chmod +x /usr/local/bin/docker-compose > /dev/null
 
-echo -e "${green}====${rst} Docker has been successfully installed ${GREEN}====${RST}\n"
+echo -e "${green}====${rst} Docker has been successfully installed ${green}====${rst}\n"
 docker --version
 echo -e "docker-compose version : $(docker-compose -v | cut -d " " -f 3,4,5)\n"
 
-echo -e "${green}====${rst} Creating docker dedicated user ${GREEN}====${RST}\n"
+echo -e "${green}====${rst} Creating docker dedicated user ${green}====${rst}\n"
 sudo groupadd docker 1> /dev/null 2>> $errfile
 sudo usermod -a -G docker $USER 1> /dev/null 2>> $errfile
 check-err
